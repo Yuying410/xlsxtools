9f924488b120046d869d4fa91a1116ac6198ffca
yuying <annie861029@gmail.com>
2024-07-22 17:34:44 +0800

    讀檔工具:未修改好(少print檔案名)

--- R/read_xlsx.R
+++ R/read_xlsx.R
@@ -1,3 +1,17 @@
-a <- function(path){
-	paste(path, path)
+# Goal 1：read xlsx file
+# Goal 2：output need sheet name,column name
+# Goal 3：data type is as.tibble
+
+# def of function
+read_xlsx_tool <- function(path){
+	#列出目錄下all file name，pattern = "\\.xlsx$ 限制只列出xlsx資料
+	file_name <- list.files(path, pattern = "\\.xlsx$", full.names = TRUE)
+
+	#read file 批次讀取所有 xlsx file
+	data_list <- lapply(file_name, function(file){
+		file_name <- basename(file)  # basename 截資料名稱
+		data <- readxl::read_xlsx(file_name)  #讀xlsx檔案
+	})
+	paste("Sheet name:",basename(file_name)) #未改好(少file name)
+	print(lapply(data_list,names))
 }
--- dev/read_xlsx_dev.R
+++ dev/read_xlsx_dev.R
@@ -1,2 +1,17 @@
-a("test")
-a("path")
+read_xlsx_tool("C:/Users/annie/Documents/git/olist_data")
+readxl::read_xlsx("olist_customers_dataset")
+
+
+##test
+path <- "C:/Users/annie/Documents/git/olist_data"
+file_name <- list.files(path, pattern = "\\.xlsx$", full.names = TRUE)
+data_list <- lapply(file_name, function(file){
+	file_name <- basename(file)
+	readxl::read_xlsx(file_name)
+})
+
+print(data_list)
+
+
+lapply(data_list,names)
+print(lapply(data_list,names))
